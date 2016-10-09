16e69d92061b8ebc6bc834e656b544a405b9bf0f
xmark2 <xmark2@gmail.com>
2016-10-09 16:08:14 +0100

    csv to json converter

---
+++ csv_to_json.py
@@ -0,0 +1,43 @@
+import csv
+import codecs
+import json
+
+def convert_to_json(input_file_name, out_file_name):
+    with codecs.open(input_file_name, encoding="utf8") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        header = []
+        rows = []
+
+        try:
+            for row in readCSV:
+                rows.append(row)
+        except UnicodeDecodeError:
+            pass        
+
+        header = rows[0]
+
+        rows.remove(header)
+        rows.sort()
+
+    data_dic = []
+
+    maxRows = len(rows)
+    maxHeader = len(header)
+
+    for i in range(0,maxRows):
+
+        my_dict = {'id':i}
+        for j in range(0,maxHeader):
+            my_dict[header[j]] = rows[i][j]
+        data_dic.append(my_dict)
+
+    out_file = open(out_file_name,"w")
+    json.dump(data_dic,out_file, indent=4)
+    out_file.close()
+
+# input_file_name = 'language_codes.csv'
+input_file_name = input('What is the name of your csv file?')
+input_file_name = input_file_name+'.csv'
+out_file_name = 'csv_output.json'
+
+convert_to_json(input_file_name, out_file_name)
