34961db615b862b9b295b8502d07c3547085eec2
John Filippone <johnpfilippone@yhaoo.com>
2016-11-05 10:59:29 -0500

    Adds c2j unittest

--- test/test_c2j.py
+++ test/test_c2j.py
@@ -1,8 +1,14 @@
 import unittest
+import sys, os
+sys.path.append(os.getcwd() + "/../src")
 from c2j import get_json_from_csv
 
-class C2VTest():
-    def test_valid_format():
-        csv_data = "name,day,id\njohn,tuesday,0\nfred,monday,1"
-        self.AssertEqual(get_json_from_csv(csv_data))
+class C2VTest(unittest.TestCase):
+    def test_valid_format(self):
+        csv_data = unicode("name,id\r\njohn,0\r\nfred,1\r\n")
+        json_data = [{"name":"john", "id":"0"}, {"name":"fred", "id":"1"}]
+        self.assertEqual(get_json_from_csv(csv_data), json_data)
+
+if __name__ == "__main__":
+    unittest.main()
 
